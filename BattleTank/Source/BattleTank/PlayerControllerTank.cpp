c6bfb4a238711e1b7d1c6e6c12003f9b21d5544a
Marco Rinaldi <marco.rinaldi9@live.com>
2018-10-12 19:21:40 +0200

    Deprojection from the crosshair screen location to World 3D

--- BattleTank/Source/BattleTank/PlayerControllerTank.cpp
+++ BattleTank/Source/BattleTank/PlayerControllerTank.cpp
@@ -3,11 +3,11 @@
 #include "PlayerControllerTank.h"
 
 
-void APlayerControllerTank::BeginPlay() 
+void APlayerControllerTank::BeginPlay()
 {
 	Super::BeginPlay();
 	ATank* TankPossessed = GetControlledTank();
-	UE_LOG(LogTemp,Warning,TEXT("%s"),*TankPossessed->GetName())
+	UE_LOG(LogTemp, Warning, TEXT("%s"), *TankPossessed->GetName())
 }
 
 void APlayerControllerTank::Tick(float DeltaTime)
@@ -26,6 +26,38 @@ void APlayerControllerTank::AimTowardsCrosshair()
 {
 	if (!GetControlledTank()) { return; }
 
+	FVector OutHitLocation;
+	if (GetSightRayHitLocation(OutHitLocation))
+	{
+		//UE_LOG(LogTemp, Warning, TEXT("%s"), *OutHitLocation.ToString())
+	}
+
+
+}
+
+bool APlayerControllerTank::GetSightRayHitLocation(FVector& OutHitLocation) const
+{
+	int32 ViewportSizeX, ViewportSizeY;
+	FVector OutWorldDirection;
+
+	GetViewportSize(ViewportSizeX, ViewportSizeY);
+	//Position of crosshair in screen(Pixel coordinate)
+	auto ScreenLocationCrossHair = FVector2D(ViewportSizeX * CrossHairXLocation, ViewportSizeY * CrossHairYLocation);
+
+	if (GetLookDirection(ScreenLocationCrossHair, OutWorldDirection))
+	{
+		UE_LOG(LogTemp,Warning,TEXT("I'm looking at: %s"),*OutWorldDirection.ToString())
+	}
+
+	return true;
+}
+
+bool APlayerControllerTank::GetLookDirection(FVector2D ObjScreenLocation,FVector& LookDirection ) const
+{
+	//Parameters to be discarded because is the camera position
+	FVector OutWorldLocation = FVector::ZeroVector; 
+	//"De-Projection" from Crosshair position in screen to World 3D
+	return DeprojectScreenPositionToWorld(ObjScreenLocation.X, ObjScreenLocation.Y, OutWorldLocation, LookDirection);
 }
 
 
--- BattleTank/Source/BattleTank/PlayerControllerTank.h
+++ BattleTank/Source/BattleTank/PlayerControllerTank.h
@@ -5,25 +5,33 @@
 #include "CoreMinimal.h"
 #include "Tank.h"
 #include "GameFramework/PlayerController.h"
+#include "Engine/World.h"
+#include "DrawDebugHelpers.h"
 #include "PlayerControllerTank.generated.h"
 
 /**
- * 
+ *
  */
 UCLASS()
 class BATTLETANK_API APlayerControllerTank : public APlayerController
 {
 	GENERATED_BODY()
 
-	
+
 public:
 	virtual void BeginPlay() override;
 	virtual void Tick(float DeltaTime) override;
 	ATank* GetControlledTank() const;
-	
+
 
 private:
 	void AimTowardsCrosshair();
+	UPROPERTY(EditAnywhere)
+	float CrossHairXLocation = 0.5f;
+	UPROPERTY(EditAnywhere)
+	float CrossHairYLocation = 0.23111f;
+	bool GetSightRayHitLocation(FVector& OutHitLocation) const;
+	bool GetLookDirection(FVector2D ObjScreenLocation,FVector& LookDirection) const;
 
-	
+
 };
